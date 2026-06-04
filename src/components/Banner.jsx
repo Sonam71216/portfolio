6edd67e92effdc78e3184c0e12169c0f04c974dd
Sonam71216 <snm71216@gmail.com>
2026-06-04 21:32:59 +0530

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,12 +3,12 @@ import profileImg from "../assets/images/profile1.png";
 
 function Banner() {
   return (
-    <section className="bnr-sec bg-[var(--main-bg-color)] text-[var(--main-text-color)] md:p-0 px-4" id='banner' >
+    <section className="bnr-sec bg-[var(--main-bg-color)] text-[var(--main-text-color)] md:px-6 lg:px-4 px-6 px-4" id='banner' >
     <div className='max-w-7xl mx-auto'>
     <div className="flex justify-center items-center">
     <div className="mainTitle py-9 ">
       <p className='top-line'>HELLO, I'M SONAM</p>
-      <h1 className='title text-[60px]'>Frontend Developer & <span>UI/UX </span>Designer</h1>
+      <h1 className='title  text-[35px] md:text-[60px]'>Frontend Developer & <span>UI/UX </span>Designer</h1>
       <p className='pt-4'>I deliver user-focused designs and scalable web solutions that enhance user experience, improve performance, and drive meaningful business results effectively.</p>
     <button className='bg-[var(--main-btn-color)] p-[10px_25px] my-6 mr-2 text-[var(--btn-text-color)] rounded-sm border-1
     hover:border-1 hover:bg-transparent hover:text-[var(--main-text-color)] rounded-md hover:bg-transparent transition duration-500'> <a href="#contact">View My Work</a></button>
